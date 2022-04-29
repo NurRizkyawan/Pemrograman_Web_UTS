ae69936b0707108e2402e4231dfc3029a6caa5e3
NurRizkyawan <104565967+NurRizkyawan@users.noreply.github.com>
2022-04-29 07:41:59 +0700

    Add files via upload

---
+++ Sekolah.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="style.css">
+    <title>Pengalaman Saya</title>
+    </head>
+
+
+    <body>
+        <?php
+        echo "Sekolah :
+                <ol>
+                    <li>SD <br>
+                    SD saya di SD Negeri Bandut, kenangan yang masih aku ingat
+                    adalah guru kelas satu nya galak kepada murid-muridnya</li>
+
+                    <br><li>SMP<br>
+                    SMP saya di SMP Negeri 2 Sedayu, saat itu saya belum lama bisa naik motor
+                    dan saya pun ke sekolah naik motor sendiri meskipun belum memiliki SIM</li>
+                    
+                    <br><li>SMK <br>
+                    SMK saya di SMK Negeri 1 Sedayu, lokasi SMK dengan rumah saya lumayan dekat.
+                    Sekolah di SMK lebih sering praktek, membuat saya jarang belajar materi</li>
+                </ol>"
+                ?>
+        <?php
+        echo "<br><br>untuk kembali tekan
+        <a href='http://localhost/PemWeb/UTS/Biodata.php' targer=_top>disini</a>"
+        ?> 
+    </body>
