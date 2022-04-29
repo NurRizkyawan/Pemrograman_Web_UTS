01fbf83c38be3271dd2a8ec66008e776d4369911
NurRizkyawan <104565967+NurRizkyawan@users.noreply.github.com>
2022-04-29 07:41:34 +0700

    Add files via upload

---
+++ Biodata.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>UTS Pemrograman Web</title>
+        </head>
+    </head>
+    <center>
+    <body>
+        <table width="800px" border="1">
+            <caption><h2> Biodata Saya </h2></caption>
+            <tr>
+                <td width="300px">Nama Lengkap</td>
+                <td width="500px">Nur Rizkyawan Maulana</td>
+            </tr>
+            <tr>
+                <td>Tempat, Tanggal Lahir</td>
+                <td>Bantul, 3 April 2000</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>Perum Sedayu Permai,Pereng Wetan Argorejo Sedayu Bantul</td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>Laki-Laki</td>
+            </tr>
+            <tr>
+                <td>Status</td>
+                <td>Belum Menikah</td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td>
+                <td>Pelajar / Mahasiswa</td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>Islam</td>
+            </tr>
+            <tr>
+                <td>Berat Badan</td>
+                <td>50 kg</td>
+            </tr>
+            <tr>
+                <td>tinggi badan</td>
+                <td>155 cm</td>
+            </tr>
+            <tr>
+                <td>Hobi</td>
+                <td>Bermain Game</td>
+            </tr>
+            <tr>
+                <td>Nomor WA</td>
+                <td>087725703669</td>
+            </tr>
+            </table>
+
+            <br>
+            <p>Foto:</p>
+            <img title="Foto Saya" src="Aku.png" width="250" heigh="250" border="5">
+            <br>
+            <br>
+        
+            <?php
+            echo "Pengalaman saya :
+            <a href='http://localhost/PemWeb/UTS/Pengalaman.php' targer=_top>disini</a>"
+                ?>
+            <?php
+            echo "<br><br>Sekolah saya :
+            <a href='http://localhost/PemWeb/UTS/Sekolah.php' targer=_top>disini</a>"
+                ?> 
+            </center>  
+</body>
+</html>
